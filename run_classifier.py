91cbde6952553eb46982ffc090cafdd54b658c50
Junha <sharpeeee@naver.com>
2019-11-09 14:49:34 +0900

    m

--- run_classifier.py
+++ run_classifier.py
@@ -874,6 +874,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
         accuracy = tf.metrics.accuracy(
             labels=label_ids, predictions=predictions, weights=is_real_example)
         loss = tf.metrics.mean(values=per_example_loss, weights=is_real_example)
+
         confusion_matrix = tf.math.confusion_matrix(label_ids, predictions)
         return {
             "eval_accuracy": accuracy,
